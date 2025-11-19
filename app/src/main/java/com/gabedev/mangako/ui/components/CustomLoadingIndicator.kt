e6bd9ec27bccf0c8b1ced92b77b4008ecb860c24
gab3-dev <gabe.dev@icloud.com>
2025-11-19 16:11:45 -0300

    feat: rename CircularLoadingIndicator to CustomLoadingIndicator and update loading indicator color to use contained color
    Signed-off-by: gab3-dev <gabe.dev@icloud.com>

--- app/src/main/java/com/gabedev/mangako/ui/components/CustomLoadingIndicator.kt
+++ app/src/main/java/com/gabedev/mangako/ui/components/CustomLoadingIndicator.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.LoadingIndicatorDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -13,6 +14,7 @@ fun CustomLoadingIndicator(
     modifier: Modifier = Modifier
 ) {
     LoadingIndicator(
+        color = LoadingIndicatorDefaults.containedContainerColor,
         modifier = modifier
             .fillMaxWidth(0.3f)
             .fillMaxHeight(0.3f),
