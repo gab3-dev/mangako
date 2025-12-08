696e82e24db0b1b12008cc0471e4a0a1ac91d4ed
gab3-dev <gabe.dev@icloud.com>
2025-12-08 16:14:58 -0300

    feat: update loading indicator size in MangaSearchList for better visibility
    Signed-off-by: gab3-dev <gabe.dev@icloud.com>

--- app/src/main/java/com/gabedev/mangako/ui/screens/search_list/MangaSearchList.kt
+++ app/src/main/java/com/gabedev/mangako/ui/screens/search_list/MangaSearchList.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -173,10 +175,15 @@ fun MangaSearchScreen(
                                 item {
                                     Box(
                                         modifier = Modifier
+                                            .fillMaxWidth()
                                             .padding(vertical = 16.dp),
                                         contentAlignment = Alignment.Center
                                     ) {
-                                        CustomLoadingIndicator()
+                                        CustomLoadingIndicator(
+                                            Modifier
+                                                .width(50.dp)
+                                                .height(50.dp)
+                                        )
                                     }
                                 }
                             }
