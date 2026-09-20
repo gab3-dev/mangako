accad4defd77f632e6ac3bf46971f26a61ac5e84
gab3-dev <gabe.dev@icloud.com>
2026-09-20 17:49:37 -0300

    chore: release version 0.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,8 +96,8 @@ android {
         applicationId = "com.gabedev.mangako"
         minSdk = 25
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "com.gabedev.mangako.MangaKoE2ETestRunner"
         buildConfigField(
